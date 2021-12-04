9360a5af7042b470cb8fad132dff26b930e6d1d9
Mouhamad ND. THIAM <thiamouhamadpro@gmail.com>
2021-12-04 17:23:20 +0000

    Implement encoding negative integers

--- py_message_encoder/utilities.py
+++ py_message_encoder/utilities.py
@@ -14,21 +14,37 @@ class CustomIntegerBase:
         self.alphabet_len = len(alphabet)
 
     def encode(self, number: int) -> str:
+        is_negative = False
+        if number < 0:
+            is_negative = True
+            number *= -1
         if number == 0:
-            return "0"
+            return self.zero
         result = ""
         while number != 0:
             number, q = divmod(number, self.alphabet_len)
             result = self.alphabet[q] + result
+        if is_negative:
+            result = self.one + result
+        elif result[0] == self.one:
+            result = self.zero + result
         return result
 
     def decode(self, encoded: str) -> int:
+        if encoded == "":
+            pass
         result = 0
+        is_negative = False
+        if encoded[0] == self.one:
+            is_negative = True
+            encoded = encoded[1:]
         for c in encoded:
             index = self.alphabet.index(c)
             if index == -1:
                 raise ValueError(f"invalid character {c}")
             result = result * self.alphabet_len + index
+        if is_negative:
+            result *= -1
         return result
 
     def __call__(self, number):
