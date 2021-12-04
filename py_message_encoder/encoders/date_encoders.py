52b86075775bd12fb8941af2303be5f6925574cb
Mouhamad ND. THIAM <thiamouhamadpro@gmail.com>
2021-12-04 23:23:00 +0000

    Implement date time encoder

--- py_message_encoder/encoders/date_encoders.py
+++ py_message_encoder/encoders/date_encoders.py
@@ -7,6 +7,7 @@ from py_message_encoder.message_types import MessageType
 from py_message_encoder.utilities import custom_base_64
 
 REFERENCE_DATE: date = date.fromtimestamp(0)
+REFERENCE_DATETIME = datetime.fromtimestamp(0)
 
 
 class DateEncoder(PartialEncoder):
@@ -14,7 +15,6 @@ class DateEncoder(PartialEncoder):
         super(DateEncoder, self).__init__(MessageType.DATE)
 
     def encode(self, value: date) -> str:
-        value.ctime()
         total_days = (value - REFERENCE_DATE).days
         return big_int.encode(total_days)
 
@@ -30,20 +30,11 @@ class TimeEncoder(PartialEncoder):
 
     def encode(self, value: time) -> str:
         seconds = value.hour * 3600 + value.minute * 60 + value.second
-        # supports offset ranging from -12 to +15 (+14 + DST)
-        # offset should multiplier of 0.5 hours (xx hours or xx.5 hours only are supported)
-        offset = value.utcoffset()
-        if offset:
-            offset //= timedelta(minutes=30)
-        else:
-            offset = 0
-        offset += 24  # normalize to avoid negative numbers by adding 12 hours (24 x 0.5 hours)
-        return custom_base_64.encode(offset) + big_int.encode(seconds)
+        offset_encoded = encode_offset(value.utcoffset())
+        return offset_encoded + big_int.encode(seconds)
 
     def decode_value(self, value: str) -> Tuple[time, int]:
-        offset = custom_base_64.decode(value[0])
-        offset -= 24
-        offset_timedelta = timedelta(minutes=offset * 30)
+        offset_timedelta = decode_offset(value[0])
         seconds, consumed = big_int.decode_value(value[1:])
         minutes, seconds = divmod(seconds, 60)
         hours, minutes = divmod(minutes, 60)
@@ -56,11 +47,35 @@ class DateTimeEncoder(PartialEncoder):
         super(DateTimeEncoder, self).__init__(MessageType.DATE_TIME)
 
     def encode(self, value: datetime) -> str:
-        pass
+        offset_encoded = encode_offset(value.utcoffset())
+        total_seconds = int((value.replace(tzinfo=None) - REFERENCE_DATETIME).total_seconds())
+        return offset_encoded + big_int.encode(total_seconds)
 
     def decode_value(self, value: str) -> Tuple[datetime, int]:
-        pass
+        offset_timedelta = decode_offset(value[0])
+        seconds, consumed = big_int.decode_value(value[1:])
+        _date_time = REFERENCE_DATETIME + timedelta(seconds=seconds)
+        _date_time = _date_time.replace(tzinfo=timezone(offset_timedelta))
+        return _date_time, consumed + 1
+
+
+def encode_offset(offset: timedelta) -> str:
+    # supports offset ranging from -12 to +15 (+14 + DST)
+    # offset should multiplier of 0.5 hours (xx hours or xx.5 hours only are supported)
+    if offset:
+        offset //= timedelta(minutes=30)
+    else:
+        offset = 0
+    offset += 24  # normalize to avoid negative numbers by adding 12 hours (24 x 0.5 hours)
+    return custom_base_64.encode(offset)
+
+
+def decode_offset(value: str) -> timedelta:
+    offset = custom_base_64.decode(value)
+    offset -= 24
+    return timedelta(minutes=offset * 30)
 
 
 date_encoder = DateEncoder()
 time_encoder = TimeEncoder()
+date_time_encoder = DateTimeEncoder()
--- py_message_encoder/main.py
+++ py_message_encoder/main.py
@@ -1,16 +1,18 @@
-from datetime import date, time
+from datetime import date, time, datetime
 from typing import List, Tuple, Any
 
 from py_message_encoder.body import MessageBody
 from py_message_encoder.encoders import PartialEncoder
-from py_message_encoder.encoders.date_encoders import date_encoder, time_encoder
+from py_message_encoder.encoders.date_encoders import date_encoder, time_encoder, date_time_encoder, DateEncoder, \
+    DateTimeEncoder, TimeEncoder
 from py_message_encoder.encoders.float_encoders import float_encoder, FloatEncoder
-from py_message_encoder.encoders.integer_encoders import small_int, _small_int, big_int
+from py_message_encoder.encoders.integer_encoders import small_int, _small_int, big_int, IntFixedLengthEncoder, \
+    _IntFixedLengthEncoder, IntegerVarLengthEncoder
 from py_message_encoder.encoders.message_body_encoder import FieldMapping, MessageField, BodyEncoder
-from py_message_encoder.encoders.message_header_encoder import header_encoder
+from py_message_encoder.encoders.message_header_encoder import header_encoder, MessageHeaderEncoder
 from py_message_encoder.header import MessageHeader
 from .encoders.boolean_encoder import BooleanEncoder
-from .encoders.string_encoders import FixedLengthEncoder, LVAR, LLVAR, LLLVAR
+from .encoders.string_encoders import FixedLengthEncoder, LVAR, LLVAR, LLLVAR, VariableLengthEncoder
 
 
 def test():
@@ -20,26 +22,43 @@ def test():
         MessageHeader(13).set_presence(0).set_presence(6).set_presence(9).set_presence(10),
     ]
     mapping = FieldMapping([MessageField("first_name", LVAR), MessageField("last_name", LVAR), MessageField("score", FloatEncoder(2))])
-    print(f"mapping.names: {mapping.names}")
     body_encoder = BodyEncoder(mapping)
-    test_cases: List[Tuple[PartialEncoder, List[Any]]] = [
-        # (BooleanEncoder(), [True, False, 0, 1, 2, 3, -1, "str", "", [1, 2], []]),
-        # (FixedLengthEncoder(10), ["", "Hi @team", "This is a very long tutorial"]),
-        # (LVAR, ["Hi", "Hi team", "This is a very long text"]),
-        # (LLVAR, ["Hi", "Hi team", "This is a very long text", "This is a very long text" * 7]),
-        # (LLLVAR, ["Hi", "Hi team", "This is a very long text", "a long text" * 7, "This is a very long text" * 13]),
-        # (small_int, [0, 23, 56, 7, 345]),
-        # (_small_int, [0, 23, 56, 7, 345]),
-        # (big_int, [0, 7, -7, 9, 23, 56, 99, 100, 345, 999, 1000, 1999, 9999, 567893980834]),
-        # (float_encoder, [round(x, float_encoder.precision) for x in [0.0, 23434., 0.23, .23, 123.0000345, 123.124242424242424423223]]),
-        # (header_encoder, message_headers),
-        # (body_encoder, [MessageBody(mapping.names, data) for data in [
-        #     {"first_name": "Thiam", "score": 45.34}, {"first_name": "DKG", "last_name": "Cidemia"},
-        #     {"last_name": "Mouhidine", "score": 34.02, "first_name": "Pathe"},
-        #     {"score": 509.34}, {}
-        # ]]),
-        # (date_encoder, [date.fromisoformat(s) for s in ["2021-12-06", "1980-01-02", "1970-01-01", "1960-01-02"]])
+    bool_cases: List[Tuple[BooleanEncoder, List[bool]]] = \
+        (BooleanEncoder(), [True, False, 0, 1, 2, 3, -1, "str", "", [1, 2], []])
+    fixed_length_cases: List[Tuple[FixedLengthEncoder, List[str]]] = \
+        (FixedLengthEncoder(10), ["", "Hi @team", "This is a very long tutorial"])
+    l_var_cases: List[Tuple[VariableLengthEncoder, List[str]]] = \
+        (LVAR, ["Hi", "Hi team", "This is a very long text"])
+    ll_var_cases: List[Tuple[VariableLengthEncoder, List[str]]] = \
+        (LLVAR, ["Hi", "Hi team", "This is a very long text", "This is a very long text" * 7])
+    lll_var_cases: List[Tuple[VariableLengthEncoder, List[str]]] = \
+        (LLLVAR, ["Hi", "Hi team", "This is a very long text", "a long text" * 7, "This is a very long text" * 13])
+    int_fixed_length_cases: List[Tuple[IntFixedLengthEncoder, List[int]]] = \
+        (small_int, [0, 23, 56, 7, 345])
+    _int_fixed_length_cases: List[Tuple[_IntFixedLengthEncoder, List[int]]] = \
+        (_small_int, [0, 23, 56, 7, 345])
+    int_var_length_cases: List[Tuple[IntegerVarLengthEncoder, List[int]]] = \
+        (big_int, [0, 7, -7, 9, 23, 56, 99, 100, 345, 999, 1000, 1999, 9999, 567893980834])
+    float_cases: List[Tuple[FloatEncoder, List[float]]] = \
+        (float_encoder, [round(x, float_encoder.precision) for x in [0.0, 23434., 0.23, .23, 123.0000345, 123.1242424242424]])
+    header_cases: List[Tuple[MessageHeaderEncoder, List[MessageHeader]]] = \
+        (header_encoder, message_headers)
+    body_cases: List[Tuple[BodyEncoder, List[bool]]] = \
+        (body_encoder, [MessageBody(mapping.names, data) for data in [
+            {"first_name": "Thiam", "score": 45.34}, {"first_name": "DKG", "last_name": "Cidemia"},
+            {"last_name": "Mouhidine", "score": 34.02, "first_name": "Pathe"},
+            {"score": 509.34}, {}
+        ]])
+    date_cases: List[Tuple[DateEncoder, List[date]]] = \
+        (date_encoder, [date.fromisoformat(s) for s in ["2021-12-06", "1980-01-02", "1970-01-01", "1960-01-02"]])
+    time_cases: List[Tuple[TimeEncoder, List[time]]] = \
         (time_encoder, [time.fromisoformat(s) for s in ["11:23:10", "17:34:11+02:00", "23:59:59+14:00", "00:00:00-12:00"]])
+    date_time_cases: List[Tuple[DateTimeEncoder, List[datetime]]] = \
+        (date_time_encoder, [
+            datetime.fromisoformat(s) for s in ["2021-12-06 11:23:10", "2021-12-04 22:51:10-05:30"]
+        ] + [datetime.utcnow(), datetime.now()])
+    test_cases: List[Tuple[PartialEncoder, List[Any]]] = [
+        date_time_cases
     ]
     for encoder, cases in test_cases:
         print(f"\n-------------------------Testing {str(encoder)} ... -----------------------------\n")
