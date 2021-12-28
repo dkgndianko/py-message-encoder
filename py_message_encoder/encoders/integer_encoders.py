6a750ecf741d3d2104ffffcf78fff30011adf072
Mouhamad ND. THIAM <thiamouhamadpro@gmail.com>
2021-12-28 19:57:43 +0000

    test float encoder and add unsigned integers

--- py_message_encoder/encoders/integer_encoders.py
+++ py_message_encoder/encoders/integer_encoders.py
@@ -2,7 +2,7 @@ from typing import Tuple, Any
 
 from py_message_encoder.encoders.string_encoders import VariableLengthEncoder, FixedLengthEncoder
 from py_message_encoder.message_types import MessageType
-from py_message_encoder.utilities import custom_base_64
+from py_message_encoder.utilities import custom_base_64, custom_unsigned_base_64, CustomIntegerBaseMixin
 
 
 class CappedIntMixin:
@@ -44,24 +44,39 @@ class _IntFixedLengthEncoder(FixedLengthEncoder, CappedIntMixin):
         return f"Integer with fixed length encoder (max: {self.max_value}, length: {self.length()})"
 
 
-class IntFixedLengthEncoder(FixedLengthEncoder, CappedIntMixin):
-    def __init__(self, length: int):
-        super(IntFixedLengthEncoder, self).__init__(length, custom_base_64.zero)
-        _max = custom_base_64.max_encodable_with_len(length)
+class IntFixedLengthEncoderMixin(FixedLengthEncoder, CappedIntMixin):
+    def __init__(self, length: int, encoder: CustomIntegerBaseMixin = custom_base_64):
+        super(IntFixedLengthEncoderMixin, self).__init__(length, encoder.zero)
+        _max = encoder.max_encodable_with_len(length)
         CappedIntMixin.__init__(self, -_max, _max)
+        self.encoder = encoder
 
     def can_encode(self, value: Any) -> Tuple[bool, str]:
         return CappedIntMixin.can_encode(self, value)
 
     def encode_value(self, value: int) -> str:
-        return super(IntFixedLengthEncoder, self).encode_value(custom_base_64.encode(value))
+        return super(IntFixedLengthEncoderMixin, self).encode_value(self.encoder.encode(value))
 
     def decode_value(self, value: str) -> Tuple[int, int]:
-        _val, consumed = super(IntFixedLengthEncoder, self).decode_value(value)
-        return custom_base_64.decode(_val), consumed
+        _val, consumed = super(IntFixedLengthEncoderMixin, self).decode_value(value)
+        return self.encoder.decode(_val), consumed
 
     def __str__(self):
         return f"Integer base64 encoded with fixed length encoder (max: {self.max_value}, length: {self.length()})"
+
+
+class IntFixedLengthEncoder(IntFixedLengthEncoderMixin):
+    def __init__(self, length: int):
+        super(IntFixedLengthEncoder, self).__init__(length, custom_base_64)
+
+
+class UnsignedIntFixedLengthEncoder(IntFixedLengthEncoderMixin):
+    def __init__(self, length: int):
+        super(UnsignedIntFixedLengthEncoder, self).__init__(length, custom_unsigned_base_64)
+
+    def __str__(self):
+        return f"Unsigned Integer base64 encoded with fixed length encoder (max: {self.max_value}, length: " \
+               f"{self.length()}) "
 
 
 class IntegerVarLengthEncoder(VariableLengthEncoder, CappedIntMixin):
@@ -85,5 +100,6 @@ class IntegerVarLengthEncoder(VariableLengthEncoder, CappedIntMixin):
 
 
 small_int = IntFixedLengthEncoder(2)
+unsigned_tiny_int = UnsignedIntFixedLengthEncoder(1)
 _small_int = _IntFixedLengthEncoder(2)
 big_int = IntegerVarLengthEncoder()
--- py_message_encoder/utilities.py
+++ py_message_encoder/utilities.py
@@ -8,47 +8,34 @@ def right_pad(value: str, target_length: int, pad_char: str):
     return f"{value}{pad_char * diff}"
 
 
-class CustomIntegerBase:
+class CustomIntegerBaseMixin:
     def __init__(self, alphabet: str):
         self.alphabet = alphabet
         self.alphabet_len = len(alphabet)
 
     def encode(self, number: int) -> str:
-        is_negative = False
-        if number < 0:
-            is_negative = True
-            number *= -1
+        assert number >= 0, "Number should be positive"
         if number == 0:
             return self.zero
         result = ""
         while number != 0:
             number, q = divmod(number, self.alphabet_len)
             result = self.alphabet[q] + result
-        if is_negative:
-            result = self.one + result
-        elif result[0] == self.one:
-            result = self.zero + result
         return result
 
     def decode(self, encoded: str) -> int:
         if encoded == "":
             return 0
         result = 0
-        is_negative = False
-        if encoded[0] == self.one:
-            is_negative = True
-            encoded = encoded[1:]
         for c in encoded:
             index = self.alphabet.index(c)
             if index == -1:
                 raise ValueError(f"invalid character {c}")
             result = result * self.alphabet_len + index
-        if is_negative:
-            result *= -1
         return result
 
     def max_encodable_with_len(self, target_len: int) -> int:
-        return self.decode(self.alphabet[-2] + self.one * (target_len - 1))
+        return self.decode(self.one * target_len)
 
     def __call__(self, number):
         return self.encode(number)
@@ -62,7 +49,43 @@ class CustomIntegerBase:
         return self.alphabet[-1]
 
 
+class CustomIntegerBase(CustomIntegerBaseMixin):
+    def encode(self, number: int) -> str:
+        is_negative = False
+        if number < 0:
+            is_negative = True
+            number *= -1
+        if number == 0:
+            return self.zero
+        result = super(CustomIntegerBase, self).encode(number)
+        if is_negative:
+            result = self.one + result
+        elif result[0] == self.one:
+            result = self.zero + result
+        return result
+
+    def decode(self, encoded: str) -> int:
+        if encoded == "":
+            return 0
+        is_negative = False
+        if encoded[0] == self.one:
+            is_negative = True
+            encoded = encoded[1:]
+        result = super(CustomIntegerBase, self).decode(encoded)
+        if is_negative:
+            result *= -1
+        return result
+
+    def max_encodable_with_len(self, target_len: int) -> int:
+        return self.decode(self.alphabet[-2] + self.one * (target_len - 1))
+
+
+class CustomUnsignedIntegerBase(CustomIntegerBaseMixin):
+    pass
+
+
 custom_alpha = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{};'\"\\/.,?><"
 # custom_alpha = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 custom_base_64 = CustomIntegerBase(custom_alpha)
 custom_binary = CustomIntegerBase("01")
+custom_unsigned_base_64 = CustomUnsignedIntegerBase(custom_alpha)
